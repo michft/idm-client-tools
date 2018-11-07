3165be5695df2d168c40625e0b097a95f5d64a74
mister0825 <30447239+mister0825@users.noreply.github.com>
2018-11-07 10:03:43 -0800

    Add docstrings and usage comment

--- idm-client-tools.py
+++ idm-client-tools.py
@@ -1,47 +1,49 @@
 #!/usr/bin/env python
 
+# Example tasks using the lightweight FreeIPA JSON RPC client
+# Uses Python Fire to generate a CLI for the ClientTools class
+# Run with no arguments for usage help
+
 import pprint
 import fire
 from python_freeipa import Client, exceptions
-
-
-""" Example tasks using the lightweight FreeIPA JSON RPC client"""
-""" Uses Python Fire to generate a CLI for the ClientTools class """
-
 
 client = Client("ipa.demo1.freeipa.org", version="2.215")
 client.login("admin", "Secret123")
 
 
 class ClientTools:
-    def usershow(self, uid):
-        try:
-            user = client.user_show(uid)
-            pprint.pprint(user)
-        except exceptions.NotFound:
-            print("User {} not found.".format(uid))
-            exit()
+	def usershow(self, uid):
+		""" Show account details for a specified user. """
+		try:
+			user = client.user_show(uid)
+			pprint.pprint(user)
+		except exceptions.NotFound:
+			print("User {} not found.".format(uid))
+			exit()
 
-    def userstatus(self, uid):
-        try:
-            user = client.user_status(uid)
-            pprint.pprint(user)
-        except exceptions.NotFound:
-            print("User {} not found.".format(uid))
-            exit()
+	def userstatus(self, uid):
+		""" Show account status for a specified user. """
+		try:
+			user = client.user_status(uid)
+			pprint.pprint(user)
+		except exceptions.NotFound:
+			print("User {} not found.".format(uid))
+			exit()
 
-    def userfind(self):
-        userlist = []
-        allusers = client.user_find()
-        result = allusers.get('result')
-        for items in result:
-            userlist.extend(items.get('uid'))
-        print(userlist)
+	def userfind(self):
+		""" List all enrolled users. """
+		userlist = []
+		allusers = client.user_find()
+		result = allusers.get('result')
+		for items in result:
+			userlist.extend(items.get('uid'))
+		print(userlist)
 
 
 def main():
-    fire.Fire(ClientTools)
+	fire.Fire(ClientTools)
 
 
 if __name__ == '__main__':
-    main()
+	main()
